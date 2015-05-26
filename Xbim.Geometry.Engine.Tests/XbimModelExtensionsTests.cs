dc0a45dd41e46b51c640651b13e07b0ebaf949d8
Claudio Benghi <claudio.benghi@gmail.com>
2015-05-26 22:06:13 +0100

    Update references and new features for geometry extraction.

---
+++ Xbim.Geometry.Engine.Tests/XbimModelExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xbim.IO;
+
+namespace GeometryTests
+{
+    [TestClass]
+    [DeploymentItem(@"EsentTestFiles\")]
+    public class XbimModelExtensionsTests
+    {
+        [TestMethod]
+        public void CanGetRawGeometry()
+        {
+            var m = new XbimModel();
+            m.Open("Monolith_v20.xBIM");
+            Assert.AreEqual(2, m.GeometrySupportLevel, "GeometrySupportLevel for Monolith_v20 should be 2");
+            m.Close();
+        }
+    }
+}
