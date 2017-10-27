0c134022c1c3f19db262182228c97080da3104d4
Claudio Benghi <claudio.benghi@gmail.com>
2017-10-27 19:41:43 +0100

    Added occ tool (to extract breps).

---
+++ Xbim.OccTool/Commands/CommandBrep.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xbim.Common.Geometry;
+using Xbim.Common.Logging;
+using Xbim.Geometry.Engine.Interop;
+using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
+
+namespace Xbim.OccTool.Commands
+{
+    class CommandBrep : Command
+    {
+        internal static readonly ILogger Log = LoggerFactory.GetLogger();
+
+        static new string Name = "Brep";
+        internal static new string Syntax = "brep EntityLabel... FileName";
+
+        internal override string GetName()
+        {
+            return Name;
+        }
+
+        internal override string GetSyntax()
+        {
+            return Syntax;
+        }
+
+        [Flags]
+        internal enum validOptions
+        {
+            None, 
+            /// <summary>
+            /// ProgressiveFileNames
+            /// </summary>
+            PFN = 1
+        }
+
+        internal override int Process(string[] args, int index)
+        {
+            var options = validOptions.None;
+            while (index < args.Length)
+            {
+                var nm = args[index];
+                if (nm == "PFN")
+                {
+                    options |= validOptions.PFN;
+                    index++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // parse entity list
+            List<int> entities = new List<int>();
+            while (index < args.Length)
+            {
+                int el;
+                if (!int.TryParse(args[index], out el))
+                    break;
+                entities.Add(el);
+                index++;
+            }
+            if (!entities.Any())
+            {
+                Log.Error($"Not enough parameters: No entities specified.");
+                return 100;
+            }
+            
+            // parse filename
+            FileInfo IfcFile = null;
+            if (index < args.Length)
+            {
+                IfcFile = new FileInfo(args[index]);
+                if (!IfcFile.Exists)
+                {
+                    Log.Error($"File {IfcFile.FullName} not found.");
+                    return 100;
+                }
+            }
+
+            if (IfcFile == null)
+            {
+                Log.Error($"Not enough parameters: No file specified.");
+                return 100;
+            }
+
+            // we can work on the file
+            var geomEngine = new XbimGeometryEngine();
+            using (var m = IfcStore.Open(IfcFile.FullName))
+            {
+                DirectoryInfo dOut = new DirectoryInfo(Path.Combine(
+                    IfcFile.Directory.FullName,
+                    Path.GetFileNameWithoutExtension(IfcFile.FullName)
+                    ));
+                int iProgressive = 0;
+                foreach (var entityLabel in entities)
+                {
+                    iProgressive++;
+                    var ent = m.Instances[entityLabel];
+                    if (ent == null)
+                    {
+
+                        Log.Error($"Entity #{entityLabel} not found in the model.");
+                        continue;
+                    }
+                    var geomEntity = ent as IIfcGeometricRepresentationItem;
+                    if (geomEntity == null)
+                    {
+                        Log.Error($"Entity #{entityLabel} is not a valid IIfcGeometricRepresentationItem.");
+                        continue;
+                    }
+
+                    var geometryObject = geomEngine.Create(geomEntity);
+                    if (geometryObject == null)
+                    {
+                        Log.Error($"Geometry object creation failed for #{entityLabel}.");
+                        continue;
+                    }
+
+                    var brep = geomEngine.ToBrep(geometryObject);
+                    if (string.IsNullOrEmpty(brep))
+                    {
+                        Log.Error($"Empty BREP for #{entityLabel}.");
+                        continue;
+                    }
+
+                    var localFileName = entityLabel.ToString();
+                    if (options.HasFlag(validOptions.PFN))
+                    {
+                        localFileName = iProgressive.ToString();
+                    }
+
+                    var wFileName = Path.Combine(dOut.FullName , $"{localFileName}.brep");
+                    dOut.Create();
+                    var wFileInfo = new FileInfo(wFileName);
+                    using (var wFileStream = wFileInfo.CreateText())
+                    {
+                        wFileStream.WriteLine("DBRep_DrawableShape"); // required in brep file for occ
+                        wFileStream.Write(brep);
+                        wFileStream.Close();
+                        Console.WriteLine($"Element #{entityLabel} saved.");
+                    }
+                }
+            }
+            return 100;
+        }
+    }
+}
---
+++ Xbim.OccTool/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xbim.Common.Logging;
+using Xbim.OccTool.Commands;
+
+namespace Xbim.OccTool
+{
+    class Program
+    {
+        internal static readonly ILogger Log = LoggerFactory.GetLogger();
+
+        static void Main(string[] args)
+        {
+            List<Command> commands = new List<Command>()
+            {
+                new CommandHelp(),
+                new CommandBrep()
+            };
+
+            Console.WriteLine("Xbim.OccTool");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("- warning: rguments missing.");
+                DisplayHelp(commands);
+                return;
+            }
+            int index = 0;
+            while (index < args.Length)
+            {
+                var processed = false;
+                foreach (var command in commands)
+                {
+                    var cmName = command.GetName().ToLowerInvariant();
+                    if (cmName == args[index].ToLowerInvariant())
+                    {
+                        index++;
+                        // start evaluation of parameters at the next argument
+                        index += command.Process(args, index);
+                        processed = true;
+                        break;
+                    }
+                }
+                if (!processed)
+                {
+                    Log.Warn($"Parameter '{args[index]}' ignored.");
+                    index++;
+                }
+            }
+#if DEBUG
+            Console.WriteLine("Press any key");
+#else
+            Console.WriteLine("Completed.");
+#endif
+            Console.ReadKey();
+        }
+
+        private static void DisplayHelp(List<Command> commands)
+        {
+            Console.WriteLine("Syntax is: Xbim.OccTool Command");
+            Console.WriteLine("Available commands are:");
+            foreach (var command in commands)
+            {
+                Console.WriteLine($"- {command.GetSyntax()}");
+            }
+            Console.WriteLine("use 'help CommandName' for more.");
+        }
+    }
+}
