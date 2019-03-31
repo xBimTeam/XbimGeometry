81299dcba75c6e2d5679fb0168581f444aaa5da1
Claudio Benghi <claudio.benghi@gmail.com>
2019-03-31 12:35:34 +0100

    Added test (currently failing)... Instance bounding box should be in absolute coordinates.

--- Xbim.Geometry.Engine.Tests/BoundingBoxTests.cs
+++ Xbim.Geometry.Engine.Tests/BoundingBoxTests.cs
@@ -68,15 +68,41 @@ namespace GeometryTests
         }
 
         [TestMethod]
-        [DeploymentItem(@"SolidTestFiles\OneWallTwoWindows.ifc")]
+        [DeploymentItem(@"Ifc4TestFiles\grid-placement.ifc")]
+        public void GeometryInstanceBoundingBoxesMatchForGridPlacement()
+        {
+            var fileName = "grid-placement.ifc";
+            GeometryInstanceBoundingBoxMatchesOn(fileName);
+        }
+
+
+        [TestMethod]
         [DeploymentItem(@"SolidTestFiles\OneWallOneVoid.ifc")]
+        public void GeometryInstanceBoundingBoxesMatchSingleVoidedWall()
+        {
+            var fileName = "OneWallOneVoid.ifc";
+            GeometryInstanceBoundingBoxMatchesOn(fileName);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"ifc2x3Files\positionWithMappedItems.ifc")]
+        public void GeometryInstanceBoundingBoxesMatchWithMaps()
+        {
+            var fileName = "positionWithMappedItems.ifc";
+            GeometryInstanceBoundingBoxMatchesOn(fileName);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"SolidTestFiles\OneWallTwoWindows.ifc")]
         public void GeometryInstanceBoundingBoxesMatch()
         {
-            DirectoryInfo d = new DirectoryInfo(".");
-            Debug.WriteLine(d.FullName);
+            var fileName = "OneWallTwoWindows.ifc";
+            GeometryInstanceBoundingBoxMatchesOn(fileName);
+        }
 
-            FileInfo f = new FileInfo("OneWallOneVoid.ifc");
-            
+        private void GeometryInstanceBoundingBoxMatchesOn(string fileName)
+        {
+            FileInfo f = new FileInfo(fileName);
             var xbimName = CreateGeometry(f, true).FullName;
 
             using (var model = IfcStore.Open(xbimName))
@@ -86,7 +112,10 @@ namespace GeometryTests
                 {
 
                     var shapeInstances = geomReader.ShapeInstances;
-                    var FailedShapes= new List<string>();
+                    var FailedShapes = new List<string>();
+                    var FailedInstances = new List<string>();
+
+
                     foreach (var shapeInstance in shapeInstances.OrderBy(x => x.IfcProductLabel))
                     {
                         IXbimShapeGeometryData shapeGeom = geomReader.ShapeGeometry(shapeInstance.ShapeGeometryLabel);
@@ -95,7 +124,7 @@ namespace GeometryTests
 
                         var xbimMesher = new XbimMesher();
                         xbimMesher.AddMesh(shapeGeom.ShapeData);
-                        
+
                         XbimRect3D computedBox = new XbimRect3D();
                         foreach (var pnt in xbimMesher.Positions)
                         {
@@ -103,7 +132,16 @@ namespace GeometryTests
                         }
                         // Debug.WriteLine("Min: " + computedBox.Min);
                         XbimRect3D storedBox = XbimRect3D.FromArray(shapeGeom.BoundingBox);
-                        
+
+                        var instanceBox = shapeInstance.BoundingBox;
+                        var locatedBox = storedBox.Transform(shapeInstance.Transformation);
+                        if (!BoxesAreSame(instanceBox, locatedBox, model.ModelFactors.Precision))
+                        {
+                            FailedInstances.Add(shapeInstance.ToString());
+                            if (FailedInstances.Count > 10)
+                                continue; // exit the loop if many errors.
+                        }
+
                         if (!BoxesAreSame(computedBox, storedBox, model.ModelFactors.Precision))
                         {
                             FailedShapes.Add(shapeGeom.ToString());
@@ -113,6 +151,7 @@ namespace GeometryTests
                     }
 
                     Assert.AreEqual(0, FailedShapes.Count, "Shapes failing bounding box test: " + string.Join(",\r\n", FailedShapes));
+                    Assert.AreEqual(0, FailedInstances.Count, "Instances failing bounding box test: " + string.Join(",\r\n", FailedInstances));
                 }
             }
         }
