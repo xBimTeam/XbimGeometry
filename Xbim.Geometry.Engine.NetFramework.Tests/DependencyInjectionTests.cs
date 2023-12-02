c6192c18fbdce5ac4e2f4399c5d2e53bfeedd21e
Andy Ward <andy.ward@xbim.net>
2023-12-02 13:39:50 +0000

    Fix DI issue when setting up DI under net framework
    Caused by mismatch in 2.1.1 and 6.0.0 M.E.DependencyInjection

---
+++ Xbim.Geometry.Engine.NetFramework.Tests/DependencyInjectionTests.cs
@@ -0,0 +1,50 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xbim.Common.Configuration;
+using Xbim.Geometry.Abstractions;
+using Xbim.Geometry.Engine.Interop.Extensions;
+using Xbim.Ifc4.Interfaces;
+using Xunit;
+
+namespace Xbim.Geometry.Engine.NetFramework.Tests
+{
+    public class DependencyInjectionTests
+    {
+        [Fact]
+        public void CanResolveGeometryEngine()
+        {
+
+            var engine = XbimServices.Current.ServiceProvider.GetService<IXbimGeometryEngine>();
+            Assert.NotNull(engine);
+        }
+
+        [Fact]
+        public void CanResolveShapeStore()
+        {
+            var service = XbimServices.Current.ServiceProvider.GetService<IXShapeService>();
+            Assert.NotNull(service);
+        }
+
+        [Fact]
+        public void CanResolveGeometryPrimitives()
+        {
+            var service = XbimServices.Current.ServiceProvider.GetService<IXGeometryPrimitives>();
+            Assert.NotNull(service);
+        }
+
+        [Fact]
+        public void CanResolveILoggerFactory()
+        {
+            var logFactory = XbimServices.Current.ServiceProvider.GetService<ILoggerFactory>();
+
+            Assert.NotNull(logFactory);
+
+            Assert.IsType<LoggerFactory>(logFactory);
+
+            var expectedVersion = new Version(2, 1, 0,0);
+            Assert.Equal(expectedVersion, logFactory.GetType().Assembly.GetName().Version);
+        }
+
+        
+    }
+}
